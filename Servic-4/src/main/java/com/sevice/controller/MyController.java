9efb8c8f2762a2b96bf7ec9aab90846a548f3871
SzervizIvo <paradisho@gmail.com>
2018-05-01 18:14:25 +0200

    init

--- Servic-4/src/main/java/com/sevice/controller/MyController.java
+++ Servic-4/src/main/java/com/sevice/controller/MyController.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.sevice.bean.City;
+import com.sevice.bean.Device;
 import com.sevice.service.ICityService;
 
 @Controller
@@ -18,19 +17,11 @@ public class MyController {
     @Autowired
     ICityService cityService;
 
-//    @GetMapping("/deviceClient")
-//    public String findCities(Model model) {
-        
-//        List<City> cities = (List<City>) cityService.findAll();
-//        
-//        model.addAttribute("cities", cities);
-        
-//        return "deviceClient";
-//    }
-    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+
+    @RequestMapping(value = {"/deviceClient"}, method = RequestMethod.GET)
     public String tableq(Model model) {
     	
-   	List<City> cities = (List<City>) cityService.findAll();
+   	List<Device> cities = (List<Device>) cityService.findAll();
    	
   	model.addAttribute("cities", cities);
     	System.out.println(cities.toString());
@@ -39,7 +30,7 @@ public class MyController {
     @RequestMapping(value = {"/index"}, method = RequestMethod.GET)
     public String table(Model model) {
     	
-    	List<City> cities = (List<City>) cityService.findAll();
+    	List<Device> cities = (List<Device>) cityService.findAll();
     	
     	model.addAttribute("cities", cities);
     	System.out.println(cities.toString());
--- Servic-4/src/main/java/com/sevice/repository/CityRepository.java
+++ Servic-4/src/main/java/com/sevice/repository/CityRepository.java
@@ -3,9 +3,9 @@ package com.sevice.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sevice.bean.City;
+import com.sevice.bean.Device;
 
 @Repository
-public interface CityRepository extends CrudRepository<City, Long> {
+public interface CityRepository extends CrudRepository<Device, Long> {
 
 }
